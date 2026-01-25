c1a97c3b0c57ec2c40b6a3ddfca8b13ec03e0fc1
Michael Ehrlich <Michael@MichaelEhrlich.com>
2026-01-25 11:39:30 -0500

    Add dynamic config for studio route

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -3,6 +3,8 @@
 import { NextStudio } from 'next-sanity/studio'
 import config from '../../../sanity.config'
 
+export const dynamic = 'force-dynamic'
+
 export default function StudioPage() {
   return <NextStudio config={config} />
 }
